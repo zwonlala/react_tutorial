223191f77a367585c6858be899b5ebfaf4a8c7a4
jiwon_lala <s26788761@naver.com>
2020-07-30 22:00:00 +0900

    review lecture 16
    App.js에서 onToggle 구현할 때, active 값 ! 연산자 이용해서 값을 반전시킬때, active를 "active"로 써야하는지 "[active]"로 써야하는지 헷갈림
    
    UserList 컴포넌트에서 User 컴포넌트로 props 보낼때 "onToggle={onToggle}" 이렇게 보내주지 않고 "onToggle" 이렇게만 보내줘서 User 컴포넌트에서 TypeError 발생하였음!
    
    User 컴포넌트에서 style 객체 만들 때, green과 black은 "green", "black"으로 나타내야 함!
    User 컴포넌트에서 마우스 커서 손가락으로 바꾸는 "cursor: 'pointer'" 속성이 기억이 안났음

--- src/jiwon/App.js
+++ src/jiwon/App.js
@@ -4,50 +4,6 @@ import CreateUser from './CreateUser';
 
 function App() {
 
-    const [users, setUsers] = useState([
-        {
-            id: 1,
-            name: 'asdf',
-            email: 'asdf@asdf.com'
-        },
-        {
-            id: 2,
-            name: 'aaaaaa',
-            email: 'aaaaaa@aaaaa.com'
-        },
-        {
-            id: 3,
-            name: 'bbbbbb',
-            email: 'bbbbb@bbbbbb.com'
-        },
-    ]);  
-    const idCnt = useRef(4); 
-    const onCreate= () => { 
-        const newUser = {
-            id: idCnt.current,
-            name: name,
-            email: email
-        };
-
-        setUsers(
-            [...users, newUser] 
-            // users.concat(newUser)
-        );
-    
-        idCnt.current += 1;
-
-        setInput({
-            name: '',
-            email: ''
-        });
-    }
-
-    const onRemove = (id) => {
-        setUsers(
-            users.filter((user) => user.id !== id)
-        );
-    }
-
     const [input, setInput] = useState({
         name: '',
         email: ''
@@ -60,6 +16,64 @@ function App() {
             [name]: value,    
         });
     };
+
+    const [users, setUsers] = useState([
+        {
+            id: 1,
+            name: 'asdf',
+            email: 'asdf@asdf.com',
+            active: true
+        },
+        {
+            id: 2,
+            name: 'aaaaaa',
+            email: 'aaaaaa@aaaaa.com',
+            active: false
+        },
+        {
+            id: 3,
+            name: 'bbbbbb',
+            email: 'bbbbb@bbbbbb.com',
+            active: false
+        },
+    ]);  
+    const idCnt = useRef(4); 
+
+    const onCreate= () => { 
+        const newUser = {
+            id: idCnt.current,
+            name: name,
+            email: email,
+            active: false
+        };
+        setUsers(
+            [...users, newUser] 
+            // users.concat(newUser)
+        );
+        idCnt.current += 1;
+        setInput({
+            name: '',
+            email: ''
+        });
+    }
+
+    const onRemove = (id) => {
+        setUsers(
+            users.filter((user) => user.id !== id)
+        );
+    }
+
+    const onToggle = (id) => {
+        setUsers(
+            users.map(user => 
+                user.id === id //Failed to compile Error 발생
+                //Line 69:17:  Expected an assignment or function call and instead saw an expression  no-unused-expressions
+                ? {...user, active: !user.active} //여기 부분 active 업데이트 해주는 부분 헷갈림... 객체 표현 쓰는 부분...!
+                : user
+            )
+        );
+    };
+    
     
     return (
         <>
@@ -69,7 +83,7 @@ function App() {
                 onChange={onChange} 
                 onCreate={onCreate} 
             />
-            <UserList users={users} onRemove={onRemove}/>
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
         </>
     );
 }
--- src/jiwon/UserList.js
+++ src/jiwon/UserList.js
@@ -1,21 +1,27 @@
 import React, { useRef } from 'react';
 
-function User( { user, onRemove } ) { 
-    const { name, email } = user;
+function User( { user, onRemove, onToggle } ) { 
+    const { name, email, active, id } = user;
+    const nameStyle = {
+        color: active? "green" : "black", //green과 black "green" "black"으로 나타내야 함!
+        cursor: 'pointer'
+    };
     return (
-        <div>
-            <b>{name}</b> {email}
+        /* 여기 그 마우스 커서 손가락으로 바꾸는거 기억 안남. */
+        <div onClick={() => onToggle(id)} > {/*TypeError: onToggle is not a function 에러 발생*/}
+        {/* props 보내줄 때, "onToggle={onToggle}" 이렇게 보내주지 않고 "onToggle" 이렇게만 보내줘서 위 TypeError 발생하였음! */}
+            <b style={nameStyle}>{name}</b> {email}
             <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     );
 }
 
 
-function UserList( { users, onRemove }) { 
+function UserList( { users, onRemove, onToggle }) { 
     return (
         <>
             { users.map(  
-                user => <User user={user} key={user.id} onRemove={onRemove}/>   
+                user => <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>   
             )}
         </>
     );
